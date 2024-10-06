db1e8771e1301ac0ea264f07a4cb39279d17f862
Bartlomiej Drapinski <g00428194@atu.ie>
2024-10-06 19:03:18 +0100

    Added new method called subtract

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -9,17 +9,21 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         //methods
-        System.out.println("Please enter the first number: ");
+        System.out.print("Please enter the first number: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println("Please enter the first number: ");
+        System.out.print("Please enter the first number: ");
         int b = sc.nextInt();
-        System.out.println("Please enter a operation: ");
+        System.out.println("Please enter an operation: ");
+        System.out.println("add, subtract, divide, multiply, exponent");
         String op = sc.next();
 
         switch (op) {
             case "add":
                 add(a, b);
+                break;
+            case "subtract":
+                sub(a, b);
                 break;
             default:
                 System.out.println("Invalid operation");
@@ -29,5 +33,9 @@ public class Calculator {
     public static void add(int a, int b) {
         int sum = a + b;
         System.out.println("The sum is: " + sum);
+    }
+    public static void sub(int a, int b) {
+        int sub = a - b;
+        System.out.println("The answer is: " + sub);
     }
 }
