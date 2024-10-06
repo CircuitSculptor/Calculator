78afd2e4171aa6c312b541cd2275cb3545f412d5
Bartlomiej Drapinski <g00428194@atu.ie>
2024-10-06 18:11:22 +0100

    Added new method called add

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,9 +4,30 @@
  Basic Calculator in Java
 */
 
+import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        //methods
+        System.out.println("Please enter the first number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Please enter the first number: ");
+        int b = sc.nextInt();
+        System.out.println("Please enter a operation: ");
+        String op = sc.next();
 
+        switch (op) {
+            case "add":
+                add(a, b);
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+        }
+    }
+    public static void add(int a, int b) {
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
     }
 }
